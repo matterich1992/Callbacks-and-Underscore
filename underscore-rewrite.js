b00add79667e9fcd6631997e60fae2d0efdd4621
Matthew Erich <matthewerich1992@gmail.com>
2016-08-14 21:14:15 -0700

    Rewrite_underscore Library
    started with reimplementing
     ‘_.each’ and ‘_.map’

---
+++ underscore-rewrite.js
@@ -0,0 +1,29 @@
+//Reimplementing Underscore JS
+
+//Each
+// Will loop through each value in the argument and execute a callback function on that value
+
+var each = function(arr,callback){
+if(Array.isArray(arr)){		// this portion addresses that if the arg is an array, use for loop i iteration
+	for(var i =0;i<arr.length;i++){
+		callback(arr[i]);
+
+	}	
+  }
+ else{ 
+ for(var key in arr){ // this portion indicates that if there is an object as an arg, use for-in loop
+ 	callback(arr[key]);
+ 
+   }
+  } 
+}
+
+
+
+var map = function(arg, callback){
+var results = [];
+each(arg, function(val){
+results.push(callback(val));
+});
+return results;
+}
